519a1a5bdd3c8f435f3d2d86efd8d79b7d7d288d
dexhrestha <dipesh.shrestha009@gmail.com>
2024-09-09 14:37:09 +0100

    Certification : responsive added

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -3,31 +3,29 @@ import { BackgroundGradient } from "../ui/background-gradient";
 
 export default function Certifications({ certifications }) {
   return (
-    <div className="grid grid-cols-4 gap-4 sm:flex sm:flex-wrap sm:gap-6 justify-center  my-10 px-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center my-10 px-5">
       {certifications.map((cert, index) => (
-        <a href={cert.href}>
-        <BackgroundGradient
-          key={index}
-          className="rounded-[22px] min-w-[300px] max-w-[400px] min-h-[300px] max-h-[400px] p-4 sm:p-6 bg-white dark:bg-zinc-900 flex flex-col items-center"
-        >
-            <div className="flex justify-center w-[200px] h-[250px]">
-            <img
+        <a href={cert.href} key={index}>
+          <BackgroundGradient
+            className="rounded-[22px] min-w-[200px] max-w-full sm:max-w-[300px] p-4 sm:p-6 bg-white dark:bg-zinc-900 flex flex-col items-center"
+          >
+            <div className="flex justify-center w-full h-[175px] sm:h-[200px]">
+              <img
                 src={cert.image}
                 alt={cert.name}
-                className="w-full h-full object-cover"
-            />
+                className="w-full h-full object-cover rounded-lg"
+              />
             </div>
 
-          <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-            <span>Valid Until: </span>
-            <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-              {cert.date}
-            </span>
-          </button>
-        </BackgroundGradient>
+            <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+              <span>Valid Until: </span>
+              <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+                {cert.date}
+              </span>
+            </button>
+          </BackgroundGradient>
         </a>
       ))}
     </div>
-    
   );
 }
